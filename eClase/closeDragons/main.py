e86d7d12b2f3d9672dc3ef32c8635f00bd5442b0
DavidMartinezITB <david.martinez.parra.7e7@itb.cat>
2024-04-24 13:16:37 +0200

    closeAllEyes

--- eClase/closeDragons/main.py
+++ eClase/closeDragons/main.py
@@ -2,8 +2,6 @@ def main():
     try:
         with open('Dragon.in', 'r') as inp:
             data = inp.read().replace('0', 'x')
-            with open('Dragon.out', 'w') as out:
-                out.write(data)
     except:
         print('ERROR')
 
