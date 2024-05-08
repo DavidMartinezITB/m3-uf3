0c9c957b930adf6e64c8b7351525f440dbade465
D4vidMart1nez <david.martinez@barresiones.cat>
2024-05-08 21:43:01 +0200

    j

--- eClase/aemet/main.py
+++ eClase/aemet/main.py
@@ -1,11 +1,14 @@
 import xmltodict
 
-file = 'data.xml'
+FILE = 'data.xml'
 
-with open(file, 'r') as f:
-    data = f.read()
+def getXmlContent(file):
+    with open(file, 'r') as f:
+        return xmltodict.parse(f.read())
 
-tree = xmltodict.parse(data)
+data = getXmlContent(FILE)
 
-for dia in tree['root']['prediccion']['dia']:
-    print(dia)
+for dia in data['root']['prediccion']['dia']:
+    fecha = dia['@fecha']
+    prob = dia['prob_precipitacion']
+    print(fecha, prob)
